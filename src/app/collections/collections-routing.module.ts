149e960fb482928e473582f28438e6730b8b7a0b
Muhammad Idrees <Idrees@192-168-1-2.tpgi.com.au>
2023-06-10 12:20:40 +1000

    add nested routing to show nested components

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BioComponent } from './bio/bio.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CompaniesComponent } from './companies/companies.component';
 
-const routes: Routes = [{ path: '', component: CollectionsHomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BioComponent },
+      { path: 'partners', component: PartnersComponent },
+      { path: 'companies', component: CompaniesComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
