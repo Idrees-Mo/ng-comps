3084a0038ea288e9523447b643066e130cd06a0a
Muhammad Idrees <Idrees@192-168-1-2.tpgi.com.au>
2023-06-10 11:43:49 +1000

    add table component with data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './collections-home.component.html',
   styleUrls: ['./collections-home.component.css'],
 })
-export class CollectionsHomeComponent {}
+export class CollectionsHomeComponent {
+  data = [
+    { name: 'Idrees', age: 30, profession: 'software engineer' },
+    { name: 'Azhaan', age: 3, profession: 'trouble2 engineer' },
+    { name: 'Hoorain', age: 7, profession: 'trouble engineer' },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'profession', label: 'Profession' },
+  ];
+}
