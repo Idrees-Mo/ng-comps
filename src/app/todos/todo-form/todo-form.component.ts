01d5facd420ff9915e2ff4f7e3b35a420359c44f
Muhammad Idrees <idris@y7mail.com>
2023-08-15 18:45:56 +1000

    add todo form

--- src/app/todos/todo-form/todo-form.component.ts
+++ src/app/todos/todo-form/todo-form.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.css']
+  styleUrls: ['./todo-form.component.css'],
 })
 export class TodoFormComponent {
+  form = this.formBuilder.nonNullable.group({
+    title: ['', Validators.required],
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    console.warn(this.form.getRawValue());
+  }
 }
