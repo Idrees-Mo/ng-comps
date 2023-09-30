3fa013f8bca1723d31d8bb2c9c2475cd1ee30151
Muhammad Idrees <idris@y7mail.com>
2023-09-30 12:29:37 +1000

    rendering modal in the body

--- src/app/mods/model/model.component.ts
+++ src/app/mods/model/model.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './model.component.html',
   styleUrls: ['./model.component.css'],
 })
-export class ModalComponent {}
+export class ModalComponent implements OnInit {
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    // To render the modal correctly
+    document.body.appendChild(this.el.nativeElement);
+  }
+}
